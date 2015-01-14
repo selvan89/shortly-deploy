f41e283659410572ee07a385907194c7244909ad
Hack Reactor Students <accounts@hackreactor.com>
2015-01-14 15:30:22 -0800

    Added a console log to check the port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('./server-config.js');
 
-var port = 4568;
-
+var port = process.env.PORT || 5150;
 app.listen(port);
+console.log(port);
 
 console.log('Server now listening on port ' + port);
